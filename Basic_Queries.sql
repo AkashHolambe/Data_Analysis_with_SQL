aaf9236084cbc962f5082efcf043e17807b7e180
Akash Holambe <akashholambe3@gmail.com>
2023-11-01 20:16:46 +0530

    Basic Queries file one

---
+++ Basic_Queries.sql
@@ -0,0 +1,13 @@
+show DATABASES
+
+Use moviesdb
+
+
+show TABLES;
+
+# show key wrod is used to show the tables present in data set 
+
+Select * from actors;
+
+# "select * " keyword selects all the information which is present in the actors table.
+
