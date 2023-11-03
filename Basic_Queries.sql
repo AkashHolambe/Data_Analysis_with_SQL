3490db6595202ec7872db879e7d9c8f1e1d2acc6
Akash Holambe <akashholambe3@gmail.com>
2023-11-04 01:24:46 +0530

    keyWord Uses

--- Basic_Queries.sql
+++ Basic_Queries.sql
@@ -15,3 +15,30 @@ Select * from actors;
 # Drop key word is used to drop particular DATABASE  form all DATABASE.
 
 # WHERE KEY Word is used to add other conditon and find specific VALUES.
+
+# CONSISTENT means values and specific rules...like NOT NULL, UNIQUE, PRIMARY KEY etc.
+# Check CONSISTENT CHECK the values with the condition if condition statisfy then only it accept the values.
+
+select title, industry  from movies;
+
+
+
+# Suppose now we only want bollywood movies
+
+select title, industry  from movies
+WHERE industry = "BollyWood"
+
+
+# Now we wamt number of how many movies in bollywood
+
+select COUNT(*) from movies 
+WHERE industry = "BollyWood"
+
+
+# TO findout unique industry in DB
+
+SELECT DISTINCT industry from movies
+
+
+
+
